9f438dda5961dde7a2761cf0b93d1439fb4fc433
howaso623 <32297207+howaso623@users.noreply.github.com>
2017-11-11 21:51:49 +0900

    Hinadan

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Semi2017 {
+  partial class Form1 {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent() {
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.ProcessButton = new System.Windows.Forms.Button();
+      this.NextImageButton = new System.Windows.Forms.Button();
+      this.menuStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(754, 26);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 22);
+      this.fileToolStripMenuItem.Text = "File";
+      // 
+      // openToolStripMenuItem
+      // 
+      this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+      this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.openToolStripMenuItem.Text = "Open";
+      this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+      this.pictureBox1.TabIndex = 1;
+      this.pictureBox1.TabStop = false;
+      // 
+      // ProcessButton
+      // 
+      this.ProcessButton.Location = new System.Drawing.Point(658, 72);
+      this.ProcessButton.Name = "ProcessButton";
+      this.ProcessButton.Size = new System.Drawing.Size(75, 23);
+      this.ProcessButton.TabIndex = 2;
+      this.ProcessButton.Text = "Process";
+      this.ProcessButton.UseVisualStyleBackColor = true;
+      this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
+      // 
+      // NextImageButton
+      // 
+      this.NextImageButton.Location = new System.Drawing.Point(658, 43);
+      this.NextImageButton.Name = "NextImageButton";
+      this.NextImageButton.Size = new System.Drawing.Size(75, 23);
+      this.NextImageButton.TabIndex = 3;
+      this.NextImageButton.Text = "NextImage";
+      this.NextImageButton.UseVisualStyleBackColor = true;
+      this.NextImageButton.Click += new System.EventHandler(this.NextImageButton_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(754, 517);
+      this.Controls.Add(this.NextImageButton);
+      this.Controls.Add(this.ProcessButton);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button ProcessButton;
+    private System.Windows.Forms.Button NextImageButton;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semi2017 {
+  public partial class Form1 : Form {
+    //
+    // メンバ
+    //
+
+    //
+    // フィールド
+    //
+
+    //
+    // 画像の切り替え表示用インデックス
+    //
+    private int m_Index;
+
+    //
+    // 読み込んだ画像配列
+    //
+    private Bitmap[] m_InputImageArray;
+
+    //
+    // 画像のサイズ
+    //  読み込んだ画像のサイズは全て同じものとする
+    //
+    private int m_Width, m_Height;
+
+    //
+    // 画像の画素数
+    //
+    private int m_NumPixel;
+
+
+    //
+    // コンストラクタ
+    //
+    public Form1() {
+      InitializeComponent();
+
+      m_Index = 0;
+
+      m_InputImageArray = null;
+
+      m_Width = m_Height = 0;
+
+      m_NumPixel = 0;
+    }
+
+
+    //
+    // メソッド
+    //
+
+    //
+    // Openボタンクリック時に呼ばれるメソッド
+    //
+    private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+      //
+      // ダイアログを開き、指定されたフォルダパスを取得する
+      //
+
+      //
+      // フォルダパスから、フォルダ内のjpgファイルパスを全て取得する
+      //
+
+      //
+      // ビットマップ配列を生成し、全ての画像を読み込む
+      //
+
+      //
+      // 画像に関するデータの変数を格納する
+      //
+
+      //
+      // ピクチャーボックスに最初の画像を設定する
+      // 表示画像の切り替え方法
+      // 　pictureBox1.Image = [Bitmapの変数];
+      //
+    }
+
+
+    //
+    // NextImageボタンクリック時に呼ばれるメソッド
+    //
+    private void NextImageButton_Click(object sender, EventArgs e) {
+      //
+      // インデックスの変数を増加させ、表示する画像を切り替える
+      // 全ての画像を表示し終えたら最初の画像に戻る
+      //
+
+      //
+      // 表示画像の切り替え方法
+      // 　pictureBox1.Image = [Bitmapの変数];
+      //
+    }
+
+    //
+    // Processボタンクリック時に呼ばれるメソッド
+    //
+    private void ProcessButton_Click(object sender, EventArgs e) {
+      //
+      // 画像処理実行コード
+      //
+    }
+  }
+}
