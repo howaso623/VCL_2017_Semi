e0560bf9069859aac8a5fb9b8168846b8a1ed854
howaso623 <32297207+howaso623@users.noreply.github.com>
2017-11-12 22:16:22 +0900

    average_variance

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,86 +23,110 @@
     /// コード エディターで変更しないでください。
     /// </summary>
     private void InitializeComponent() {
-      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.pictureBox1 = new System.Windows.Forms.PictureBox();
-      this.ProcessButton = new System.Windows.Forms.Button();
-      this.NextImageButton = new System.Windows.Forms.Button();
-      this.menuStrip1.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-      this.SuspendLayout();
-      // 
-      // menuStrip1
-      // 
-      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ProcessButton = new System.Windows.Forms.Button();
+            this.NextImageButton = new System.Windows.Forms.Button();
+            this.Average = new System.Windows.Forms.Button();
+            this.Variance = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
-      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-      this.menuStrip1.Name = "menuStrip1";
-      this.menuStrip1.Size = new System.Drawing.Size(754, 26);
-      this.menuStrip1.TabIndex = 0;
-      this.menuStrip1.Text = "menuStrip1";
-      // 
-      // fileToolStripMenuItem
-      // 
-      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(754, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem});
-      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-      this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 22);
-      this.fileToolStripMenuItem.Text = "File";
-      // 
-      // openToolStripMenuItem
-      // 
-      this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-      this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-      this.openToolStripMenuItem.Text = "Open";
-      this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-      // 
-      // pictureBox1
-      // 
-      this.pictureBox1.Location = new System.Drawing.Point(12, 29);
-      this.pictureBox1.Name = "pictureBox1";
-      this.pictureBox1.Size = new System.Drawing.Size(640, 480);
-      this.pictureBox1.TabIndex = 1;
-      this.pictureBox1.TabStop = false;
-      // 
-      // ProcessButton
-      // 
-      this.ProcessButton.Location = new System.Drawing.Point(658, 72);
-      this.ProcessButton.Name = "ProcessButton";
-      this.ProcessButton.Size = new System.Drawing.Size(75, 23);
-      this.ProcessButton.TabIndex = 2;
-      this.ProcessButton.Text = "Process";
-      this.ProcessButton.UseVisualStyleBackColor = true;
-      this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
-      // 
-      // NextImageButton
-      // 
-      this.NextImageButton.Location = new System.Drawing.Point(658, 43);
-      this.NextImageButton.Name = "NextImageButton";
-      this.NextImageButton.Size = new System.Drawing.Size(75, 23);
-      this.NextImageButton.TabIndex = 3;
-      this.NextImageButton.Text = "NextImage";
-      this.NextImageButton.UseVisualStyleBackColor = true;
-      this.NextImageButton.Click += new System.EventHandler(this.NextImageButton_Click);
-      // 
-      // Form1
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(754, 517);
-      this.Controls.Add(this.NextImageButton);
-      this.Controls.Add(this.ProcessButton);
-      this.Controls.Add(this.pictureBox1);
-      this.Controls.Add(this.menuStrip1);
-      this.MainMenuStrip = this.menuStrip1;
-      this.Name = "Form1";
-      this.Text = "Form1";
-      this.menuStrip1.ResumeLayout(false);
-      this.menuStrip1.PerformLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Location = new System.Drawing.Point(658, 72);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(75, 23);
+            this.ProcessButton.TabIndex = 2;
+            this.ProcessButton.Text = "Process";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
+            // 
+            // NextImageButton
+            // 
+            this.NextImageButton.Location = new System.Drawing.Point(658, 43);
+            this.NextImageButton.Name = "NextImageButton";
+            this.NextImageButton.Size = new System.Drawing.Size(75, 23);
+            this.NextImageButton.TabIndex = 3;
+            this.NextImageButton.Text = "NextImage";
+            this.NextImageButton.UseVisualStyleBackColor = true;
+            this.NextImageButton.Click += new System.EventHandler(this.NextImageButton_Click);
+            // 
+            // Average
+            // 
+            this.Average.Location = new System.Drawing.Point(658, 101);
+            this.Average.Name = "Average";
+            this.Average.Size = new System.Drawing.Size(75, 23);
+            this.Average.TabIndex = 4;
+            this.Average.Text = "Average";
+            this.Average.UseVisualStyleBackColor = true;
+            this.Average.Click += new System.EventHandler(this.Average_Click);
+            // 
+            // Variance
+            // 
+            this.Variance.Location = new System.Drawing.Point(658, 130);
+            this.Variance.Name = "Variance";
+            this.Variance.Size = new System.Drawing.Size(75, 23);
+            this.Variance.TabIndex = 5;
+            this.Variance.Text = "Variance";
+            this.Variance.UseVisualStyleBackColor = true;
+            this.Variance.Click += new System.EventHandler(this.Variance_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 517);
+            this.Controls.Add(this.Variance);
+            this.Controls.Add(this.Average);
+            this.Controls.Add(this.NextImageButton);
+            this.Controls.Add(this.ProcessButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
@@ -114,6 +138,8 @@
     private System.Windows.Forms.PictureBox pictureBox1;
     private System.Windows.Forms.Button ProcessButton;
     private System.Windows.Forms.Button NextImageButton;
-  }
+        private System.Windows.Forms.Button Average;
+        private System.Windows.Forms.Button Variance;
+    }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,62 +10,67 @@ using System.Windows.Forms;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.IO;
 
-namespace Semi2017 {
-  public partial class Form1 : Form {
-    //
-    // メンバ
-    //
+namespace Semi2017
+{
+    public partial class Form1 : Form
+    {
+        //
+        // メンバ
+        //
+        private Bitmap m_AverageImage;
+        private Bitmap m_VarianceImage;
+        //
+        // フィールド
+        //
 
-    //
-    // フィールド
-    //
+        //
+        // 画像の切り替え表示用インデックス
+        //
+        private int m_Index;
 
-    //
-    // 画像の切り替え表示用インデックス
-    //
-    private int m_Index;
+        //
+        // 読み込んだ画像配列
+        //
+        private Bitmap[] m_InputImageArray;
 
-    //
-    // 読み込んだ画像配列
-    //
-    private Bitmap[] m_InputImageArray;
+        //
+        // 画像のサイズ
+        //  読み込んだ画像のサイズは全て同じものとする
+        //
+        private int m_Width, m_Height;
 
-    //
-    // 画像のサイズ
-    //  読み込んだ画像のサイズは全て同じものとする
-    //
-    private int m_Width, m_Height;
-
-    //
-    // 画像の画素数
-    //
-    private int m_NumPixel;
-
-
-    //
-    // コンストラクタ
-    //
-    public Form1() {
-      InitializeComponent();
-
-      m_Index = 0;
-
-      m_InputImageArray = null;
-
-      m_Width = m_Height = 0;
-
-      m_NumPixel = 0;
-    }
+        //
+        // 画像の画素数
+        //
+        private int m_NumPixel;
 
 
-    //
-    // メソッド
-    //
+        //
+        // コンストラクタ
+        //
+        public Form1()
+        {
+            InitializeComponent();
 
-    //
-    // Openボタンクリック時に呼ばれるメソッド
-    //
-    private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            m_Index = 0;
+
+            m_InputImageArray = null;
+
+            m_Width = m_Height = 0;
+
+            m_NumPixel = 0;
+        }
+
+
+        //
+        // メソッド
+        //
+
+        //
+        // Openボタンクリック時に呼ばれるメソッド
+        //
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
             var openFolderDialog = new CommonOpenFileDialog();
             openFolderDialog.IsFolderPicker = true;
             openFolderDialog.EnsureReadOnly = true;
@@ -130,10 +135,11 @@ namespace Semi2017 {
         }
 
 
-    //
-    // NextImageボタンクリック時に呼ばれるメソッド
-    //
-    private void NextImageButton_Click(object sender, EventArgs e) {
+        //
+        // NextImageボタンクリック時に呼ばれるメソッド
+        //
+        private void NextImageButton_Click(object sender, EventArgs e)
+        {
             //
             // インデックスの変数を増加させ、表示する画像を切り替える
             // 全ての画像を表示し終えたら最初の画像に戻る
@@ -146,16 +152,113 @@ namespace Semi2017 {
             // 　pictureBox1.Image = [Bitmapの変数];
             //
             pictureBox1.Image = m_InputImageArray[m_Index];
-    }
+        }
 
-    //
-    // Processボタンクリック時に呼ばれるメソッド
-    //
-    private void ProcessButton_Click(object sender, EventArgs e) {
-      //
-      // 画像処理実行コード
-      //
-    }
+        //
+        // Processボタンクリック時に呼ばれるメソッド
+        //
+        private void ProcessButton_Click(object sender, EventArgs e)
+        {
+            //
+            // 画像処理実行コード
+            //
+            if (m_InputImageArray == null) return;
 
-  }
+            CalcAverage();
+            pictureBox1.Image = m_AverageImage;
+            CalcVariance();
+            pictureBox1.Image = m_VarianceImage;
+        }
+
+        private void Average_Click(object sender, EventArgs e)
+        {
+            CalcAverage();
+            pictureBox1.Image = m_AverageImage;
+        }
+
+        private void Variance_Click(object sender, EventArgs e)
+        {
+            CalcVariance();
+            pictureBox1.Image = m_VarianceImage;
+        }
+
+
+        private void CalcAverage()
+        {
+            m_AverageImage = new Bitmap(m_Width, m_Height); //インスタンス化
+            var colorSum = new double[m_Width, m_Height, 3]; //色の値の合計値を保存するためのバッファ
+
+            //すべてのピクセルについて
+            for (int x = 0; x < m_Width; x++)
+            {
+                for (int y = 0; y < m_Height; y++)
+                {
+                    //RGBをそれぞれ枚数分合計
+                    for (int i = 0; i < m_InputImageArray.Length; i++)
+                    {
+                        var pixelColor = m_InputImageArray[i].GetPixel(x, y);
+                        colorSum[x, y, 0] += pixelColor.R;
+                        colorSum[x, y, 1] += pixelColor.G;
+                        colorSum[x, y, 2] += pixelColor.B;
+                    }
+                    //合計値を枚数で割り（平均），その値をm_AverageImageの画素の色にする
+                    var red = (int)(colorSum[x, y, 0] / m_InputImageArray.Length);
+                    var green = (int)(colorSum[x, y, 1] / m_InputImageArray.Length);
+                    var blue = (int)(colorSum[x, y, 2] / m_InputImageArray.Length);
+                    m_AverageImage.SetPixel(x, y, Color.FromArgb(red, green, blue));
+                }
+            }
+        }
+
+        
+        private void CalcVariance()
+        {
+            m_VarianceImage = new Bitmap(m_Width, m_Height);
+            var colorVariance = new double[m_Width, m_Height, 3];
+            var maxVariance = 0.0d;     //分散の最大値
+
+            //すべてのピクセルについて
+            for (int x = 0; x < m_Width; x++)
+            {
+                for (int y = 0; y < m_Height; y++)
+                {
+                    double redSum, greenSum, blueSum;                //平均との差の二乗の合計値を保持する一時変数
+                    redSum = greenSum = blueSum = 0;                 //↑を0で初期化
+                    var avePixel = m_AverageImage.GetPixel(x, y);      //平均の画素
+
+                    //RGBをそれぞれ枚数分合計
+                    for (int i = 0; i < m_InputImageArray.Length; i++)
+                    {
+                        var pixelColor = m_InputImageArray[i].GetPixel(x, y);
+                        redSum += (avePixel.R - pixelColor.R) * (avePixel.R - pixelColor.R);
+                        greenSum += (avePixel.G - pixelColor.G) * (avePixel.G - pixelColor.G);
+                        blueSum += (avePixel.B - pixelColor.B) * (avePixel.B - pixelColor.B);
+                    }
+
+                    //平均との差の二乗を枚数で割る(平均) = 分散
+                    colorVariance[x, y, 0] = redSum / m_InputImageArray.Length;
+                    colorVariance[x, y, 1] = greenSum / m_InputImageArray.Length;
+                    colorVariance[x, y, 2] = blueSum / m_InputImageArray.Length;
+
+                    // 分散の最大値を更新
+                    maxVariance = Math.Max(maxVariance,
+                                    Math.Max(colorVariance[x, y, 0],
+                                        Math.Max(colorVariance[x, y, 1],
+                                            colorVariance[x, y, 2])));
+                }
+            }
+
+            for (int y = 0; y < m_Height; y++)
+            {
+                for (int x = 0; x < m_Width; x++)
+                {
+                    //値を0~255に正規化して画像を生成
+                    var red = (int)(colorVariance[x, y, 0] * 255 / maxVariance);
+                    var green = (int)(colorVariance[x, y, 1] * 255 / maxVariance);
+                    var blue = (int)(colorVariance[x, y, 2] * 255 / maxVariance);
+                    m_VarianceImage.SetPixel(x, y, Color.FromArgb(red, green, blue));
+                }
+            }
+        }
+    }
 }
